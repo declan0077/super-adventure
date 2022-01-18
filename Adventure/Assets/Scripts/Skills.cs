f0b62392e9efeba472a1bf4438c14076c32ad4a0
DonaldsonDane <S207822@UOS.AC.UK>
2022-01-18 12:13:55 +0000

    Skillcheck
    Added Scripts for Skillchecks

---
+++ Adventure/Assets/Scripts/Skills.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skills : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("SCArrow"))
+        {
+            collision.gameObject.GetComponentInParent<Skillcheck>().onSuccessArea = true;
+            Debug.Log("hit");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("SCArrow"))
+        {
+            collision.gameObject.GetComponentInParent<Skillcheck>().onSuccessArea = false;
+            Debug.Log("failed");
+        }
+    }
+}
